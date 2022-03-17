d6b896e82af3158079c6c9418e190958785b49dd
Prashant <prashantsp1507@gmail.com>
2022-03-17 14:55:24 +0530

    --- Auto-Git Commit ---

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ resource "aws_instance" "jenkins" {
   ami                         = data.aws_ami.ubuntu.id
   instance_type               = var.instance_type
   associate_public_ip_address = "true"
-  key_name                    = aws_key_pair.mykey.key_name
+  key_name                    = jenkins-id
   vpc_security_group_ids      = [aws_security_group.Jenkins-SecurityGroup.id]
   subnet_id                   = var.subnet_id
   user_data                   = <<EOF
