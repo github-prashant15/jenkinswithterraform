32b72792264f43433869ca5fb377a7cd34187f21
Prashant <prashantsp1507@gmail.com>
2022-03-17 19:54:21 +0530

    --- Auto-Git Commit ---

--- main.tf
+++ main.tf
@@ -25,6 +25,7 @@ resource "aws_instance" "jenkins" {
 #!/bin/bash
 sudo apt update -y 
 sudo apt install default-jre -y
+
 wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
 sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt install jenkins -y
